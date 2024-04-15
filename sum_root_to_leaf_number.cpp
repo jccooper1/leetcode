6f5f494c7a8f0c9cedb28cbe890a9aa8a7d1b031
Chen <jianchong.22@intl.zju.edu.cn>
2024-04-15 23:24:44 +0800

    4/15

---
+++ sum_root_to_leaf_number.cpp
@@ -0,0 +1,39 @@
+/*You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int find(TreeNode*root,int ans){
+        if(!root) return 0;
+        ans=ans*10+root->val;
+        if(!root->left and !root->right) return ans;
+        return find(root->left,ans)+find(root->right,ans);
+        
+    }
+    int sumNumbers(TreeNode* root) {
+
+        return find(root,0);
+
+    }
+};
